3ce7f9792efb8648160c747f2d5d2d73e4a26c4f
Dahwi <dahwi.kim96@gmail.com>
2020-04-21 08:39:30 -0400

    show image that is represented as 3d array

--- imagerec.py
+++ imagerec.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 image = Image.open('images/dot.png')
 #spit out a correspoding array(3D) of this image above
 imageArr = np.asarray(image)
 
-print imageArr 
+plt.imshow(imageArr)
+plt.show()
