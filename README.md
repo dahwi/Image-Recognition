8e5be12f956e9baff280a2098e8978412694d3e9
Dahwi <dahwi.kim96@gmail.com>
2020-04-21 22:16:42 -0400

    created a thresholding function

--- README.md
+++ README.md
@@ -26,12 +26,14 @@ Begin to use machine learning, in form of pattern recognition, to teach our prog
 </ul>
 
 ## Problem faced
-Using default python 2.7.5 installed in Linux CentOS 7, you need to install <strong> subprocess32</strong> and <strong> tkinter</strong> manually using
+Using default python 2.7.5 installed in Linux CentOS 7, you need to install <strong> subprocess32</strong> and <strong> tkinter</strong> manually to fully operate <strong> matplotlib </strong> using
 `pip install tkinter && pip install subprocess32`
 
 If you are using CentOS 7 minimal, you need to download GNOME GUI on your CentOS 7 to display the plot using `yum groupinstall "GNOME Desktop" "Graphical Administration Tools"
-` and `ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target`
+` and `ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target`(reboot your machine afterwards)
 
 Matplotlib chooses Xwindows backend by default. You need to set matplotlib to not use the Xwindows backend using
 `echo "backend: qt4agg" > ~/.config/matplotlib/matplotlibrc
-` 
+`
+
+`ValueError: assignment destination is read-only` is raised when assigning new values to numpy array meaning that you can only access but cannot modify them. This problem is fixed by downgrading numpy using `pip install numpy==1.15.4` and setting a flag parameter using `your_array_name.setflags(write = 1)` 
--- imagerec.py
+++ imagerec.py
@@ -1,11 +1,72 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-
-image = Image.open('images/dot.png')
+image = Image.open('images/numbers/0.1.png')
 #spit out a correspoding array(3D) of this image above
 imageArr = np.asarray(image)
 
-plt.imshow(imageArr)
+
+#change the values so that each pixel in the picture will be either black or white
+def threshold(imageArray):
+	balanceArr = []
+	newArr = imageArray
+	
+	for row in imageArray:
+		for pixel in row:
+			avgNum = reduce(lambda x, y : x+y, pixel[:3])/len(pixel[:3])
+			balanceArr.append(avgNum)
+
+	balance = reduce(lambda x, y : x+y, balanceArr)/len(balanceArr)
+
+		
+	for row in newArr:
+		for pixel in row:
+			if reduce(lambda x, y : x+y, pixel[:3])/len(pixel[:3]) > balance:
+				pixel[0] = 255
+				pixel[1] = 255
+ 				pixel[2] = 255
+				pixel[3] = 255
+			else:
+				pixel[0] = 0
+				pixel[1] = 0
+				pixel[2] = 0
+				pixel[3] = 255
+
+	return newArr
+
+
+image1 = Image.open('images/numbers/0.1.png')
+imageArr1 = np.asarray(image)
+
+image2 = Image.open('images/numbers/y0.4.png')
+imageArr2 = np.asarray(image2)
+imageArr2.setflags(write = 1)
+
+image3 = Image.open('images/numbers/y0.5.png')
+imageArr3 = np.asarray(image3)
+imageArr3.setflags(write = 1)
+
+
+image4 = Image.open('images/sentdex.png')
+imageArr4 = np.asarray(image4)
+imageArr4.setflags(write = 1)
+
+threshold(imageArr2)
+threshold(imageArr3)
+threshold(imageArr4)
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6),(0,0),rowspan = 4, colspan = 3)
+ax2 = plt.subplot2grid((8,6),(4,0),rowspan = 4, colspan = 3)
+ax3 = plt.subplot2grid((8,6),(0,3),rowspan = 4, colspan = 3)
+ax4 = plt.subplot2grid((8,6),(4,3),rowspan = 4, colspan = 3)
+
+ax1.imshow(imageArr1)
+ax2.imshow(imageArr2)
+ax3.imshow(imageArr3)
+ax4.imshow(imageArr4)
+
 plt.show()
+	
